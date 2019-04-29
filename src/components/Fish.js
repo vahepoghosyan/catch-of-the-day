c766f85788374e331b7a7b01c1b6bfe3c55b3c51
Vahe Poghosyan <vahe.poghosyan.js@gmail.com>
2019-04-29 09:34:36 +0400

    16 - Updating state / 0.5

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,7 +4,7 @@ import {formatPrice} from "../helpers";
 class Fish extends Component {
     render() {
         const {image, name, price, desc, status} = this.props.details;
-
+        const isAvailable = status === 'available';
         return (
             <li className='menu-fish'>
                 <img src={image} alt={image}/>
@@ -12,7 +12,7 @@ class Fish extends Component {
                     <span className="price">{formatPrice(price)}</span>
                 </h3>
                 <p>{desc}</p>
-                <button>Add To Cart</button>
+                <button disabled={!isAvailable}>{isAvailable ? 'Add To Order' : 'Sold Out'}</button>
             </li>
         );
     }
